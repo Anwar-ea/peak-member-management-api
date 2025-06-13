b82583f9c3b271f8d66ec1416490b5fccff7775e
Anwar-ea <anwar.stud.ea@gmail.com>
2025-06-13 11:47:16 +0500

    jwt fix

--- src/utility/jwt-utility.ts
+++ src/utility/jwt-utility.ts
@@ -1,12 +1,13 @@
 import { sign, verify } from "jsonwebtoken";
 import { ITokenUser } from "../models";
 import { config } from "dotenv";
+import ms from 'ms';
 config()
-export const signJwt = (user: ITokenUser, tokenSecret: string | null = null, expiryTime: string | null = null): string => {        
-    return sign(user, tokenSecret ?? (process.env.TOKEN_SECRET_KEY ?? '') , { expiresIn: expiryTime ?? '3 hours' });
+export const signJwt = (user: ITokenUser, tokenSecret: string | null = null, expiryTime: ms.StringValue | null = null): string => {        
+    return sign(user, tokenSecret ?? (process.env.TOKEN_SECRET_KEY ?? '') , { expiresIn: expiryTime ?? '3Hours' });
 }
-export const signVerificationJwt = (data: Record<string, unknown>, tokenSecret: string | null = null, expiryTime: string | null = null): string => {        
-    return sign(data, tokenSecret ?? (process.env.TOKEN_SECRET_KEY ?? '') , { expiresIn: expiryTime ?? '3 hours' });
+export const signVerificationJwt = (data: Record<string, unknown>, tokenSecret: string | null = null, expiryTime: ms.StringValue | null = null): string => {        
+    return sign(data, tokenSecret ?? (process.env.TOKEN_SECRET_KEY ?? '') , { expiresIn: expiryTime ?? '3Hours' });
 }
 
 export const verifyJwt = (jwt: string, tokenSecret: string | null = null, ignoreExpiration: boolean = false): boolean => {
